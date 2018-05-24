d94278625643b2d0e814f39c2b54fca32ce94da6
James Taylor <jimtaylor@tamu.edu>
2018-05-24 11:04:30 -0500

    Update ballgown analysis.R

--- ballgown analysis.R
+++ ballgown analysis.R
@@ -11,7 +11,7 @@ library(devtools)
 #set working directory to wherever your data is
 #setwd(C:/Users/Jim/Desktop)
 
-#load phenotype data file
+#load phenotype data file. The second line avoids an error saying the names are in the wrong order. 
 pheno_data = read.csv("phenodata.csv")
 pheno_data = pheno_data[order(pheno_data$id),]
 
@@ -23,8 +23,8 @@ bg = ballgown(dataDir = "ballgown", samplePattern = "W", pData= pheno_data)
 bg_filt = subset(bg, "rowVars(texpr(bg)) >1",genomesubset = TRUE)
 
 #statistical tests
-results_transcripts = stattest(bg_filt, feature="transcript",covariate = "treatment",getFC = TRUE, meas = "FPKM")
-results_genes = stattest(bg_filt, feature="gene", covariate = "treatment", getFC = TRUE, meas = "FPKM")
+results_transcripts = stattest(bg_filt, feature="transcript",covariate = "time",getFC = TRUE, meas = "FPKM")
+results_genes = stattest(bg_filt, feature="gene", covariate = "time", getFC = TRUE, meas = "FPKM")
 
 #add gene names and id to results transcripts
 results_transcripts = data.frame(transcriptNames=ballgown::transcriptNames(bg_filt),geneNames=ballgown::geneNames(bg_filt), geneIDs=ballgown::geneIDs(bg_filt),results_transcripts)
@@ -37,11 +37,14 @@ results_genes = arrange(results_genes,pval)
 #also I changed the title of the transcript column to transcriptNames to make life easier and so it matched the ballgown jargon
 #mart_export <-read_excel("/path/to/excel/file.xls")
 
-results_transcripts$JGI <- mart_export$GeneID[match(transcripts_6h$transcriptNames,mart_export$transcriptNames)]
+results_transcripts$JGI <- mart_export$GeneID[match(results_transcripts$transcriptNames,mart_export$transcriptNames)]
 
-write.csv(results_transcripts,"results_transcripts.csv",row.names=FALSE)
-write.csv(results_genes,"results_genes.csv",row.names = FALSE)
+write.csv(results_transcripts,"results_transcripts-time.csv",row.names=FALSE)
+write.csv(results_genes,"results_genes-time.csv",row.names = FALSE)
 
 fpkm= texpr(bg,meas = "FPKM")
 fpkm = log2(fpkm+1)
+
+# plot fpkm
+#plot(fpkm[id,]~pheno_data$variable, main=ballgown::transcriptNames(bg)[id],pch=19, xlab=variable, ylab = 'log2(FPKM+1)')
 
